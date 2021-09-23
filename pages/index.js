4a9a34d50bb0a96ae0388132dc19cd6ce76484dc
ilci66 <akbiyik.ilker7@gmail.com>
2021-09-23 11:56:19 +0300

    added recipe card component

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,30 @@
-import { createClient } from 'contentful';
+import { createClient } from 'contentful'
+import RecipeCard from '../components/RecipeCard'
 
 export async function getStaticProps() {
 
-  // this is necessary for connecting 
   const client = createClient({
     space: process.env.CONTENTFUL_SPACE_ID,
-    accessToken: process.env.CONTENTFUL_ACCESS_KEY
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
   })
 
-  // this one's for getting the items from contentful, 
-  // gotta specify the type of content you wanna get
-  const res = await client.getEntries({ content_type: 'recipe' })
-
+  const res = await client.getEntries({ content_type: "recipe" })
 
   return {
-    props:{
-      recipes: res.items
+    props: {
+      recipes: res.items,
     }
   }
-
 }
-
-
 
 export default function Recipes({ recipes }) {
   console.log(recipes)
+
   return (
     <div className="recipe-list">
-      Recipe List
+      {recipes.map(recipe => (
+        <RecipeCard key={recipe.sys.id} recipe={recipe} />
+      ))}
     </div>
   )
 }
